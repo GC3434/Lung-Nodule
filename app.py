3c15fc1609e54aa8873cf114029dbb4fdc16be00
unknown <abhisk34bahadurkar34@gmail.com>
2024-05-01 15:31:13 +0530

    Deployment ready

--- app.py
+++ app.py
@@ -16,7 +16,7 @@ from tensorflow.keras.layers import Input, concatenate
 
 app = Flask(__name__)
 
-# Roboflow setup
+# Roboflow setup last commit
 roboflow_api_key = "gJ5NmruB1DqLsgYxaPAl"   
 rf = Roboflow(api_key=roboflow_api_key)
 project = rf.workspace().project("lung-nodule-z4exc")
@@ -73,16 +73,16 @@ def analyze_image():
      # Get the accuracy for the predicted class
     accuracy = predictions[0][predicted_class_index]   
     # Run prediction on the uploaded image using Roboflow model
-    prediction = model3.predict(img_path, confidence=40, overlap=30)
+    prediction = model3.predict(img_path, confidence=40, overlap=30) last commit
 
-    # Visualize the prediction and save the image
+    # # Visualize the prediction and save the image
     pred_path = "static/images/prediction.jpg"
     prediction.save(pred_path)
 
     # Use the analyze_and_plot function to handle Tkinter in a separate function
     uploaded_plot_path = analyze_and_plot(img_path, predicted_classes[0], accuracy)
 
-    return render_template('index.html', img_path=img_path, result=predicted_classes[0], accuracy=accuracy, uploaded_plot=uploaded_plot_path, prediction=pred_path)
+    return render_template('index.html', img_path=img_path, result=predicted_classes[0], accuracy=accuracy, uploaded_plot=uploaded_plot_path)
 
 @app.after_request
 def cleanup(response):
@@ -90,4 +90,4 @@ def cleanup(response):
     return response
 
 if __name__ == '__main__':
-    app.run(port=3000, debug=True)
+    app.run(host="0.0.0.0",port=3000, debug=True)
